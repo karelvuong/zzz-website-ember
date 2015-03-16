3efecae4520fac0d71114046b0561a0aeb928625
Karel Vuong <vuongkarel@gmail.com>
2015-03-15 22:38:55 -0700

    Correct route

--- tests/unit/pods/page-posts/post-design-maker/route-test.js
+++ tests/unit/pods/page-posts/post-design-maker/route-test.js
@@ -3,7 +3,7 @@ import {
   test
 } from 'ember-qunit';
 
-moduleFor('route:posts.post-design-maker', {
+moduleFor('route:page-posts.post-design-maker', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
--- tests/unit/pods/page-posts/post-website-redesign/route-test.js
+++ tests/unit/pods/page-posts/post-website-redesign/route-test.js
@@ -3,7 +3,7 @@ import {
   test
 } from 'ember-qunit';
 
-moduleFor('route:posts.post-website-redesign', {
+moduleFor('route:page-posts.post-website-redesign', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
