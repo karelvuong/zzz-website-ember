e96d662f00ef966f8192a0675516d20d9db7d2cd
Karel Vuong <vuongkarel@gmail.com>
2015-03-17 23:53:17 -0700

    Update Quora post titles and posts page responsiveness

--- app/pods/post/model.js
+++ app/pods/post/model.js
@@ -41,7 +41,7 @@ Post.reopenClass({
     },
     {
       id: 4,
-      title: 'Should I go to University of Waterloo for CS (non coop) or University of Toronto CS (with optional PEY) for a career in the silicon valley?',
+      title: 'Should I study CS at University of Waterloo or the University of Toronto to get a career in Silicon Valley?',
       url: 'https://www.quora.com/Should-I-go-to-University-of-Waterloo-for-CS-non-coop-or-University-of-Toronto-CS-with-optional-PEY-for-a-career-in-the-silicon-valley/answer/Karel-Vuong',
       type: 2,
       date: 'Mar 17, 2015',
@@ -49,23 +49,19 @@ Post.reopenClass({
     },
     {
       id: 5,
-      title: 'How can I make the most of my computer science program at U of T?',
+      title: 'How can I make the most of my CS program at the University of Toronto?',
       url: 'https://www.quora.com/How-can-I-make-the-most-of-my-computer-science-program-at-U-of-T/answer/Karel-Vuong',
       type: 2,
       date: 'Jan 27, 2015',
-      tags: [
-        "Design Analysis"
-      ]
+      tags: []
     },
     {
       id: 6,
-      title: 'I study CS bachelor\'s at U of T, and I am close to graduating with a low GPA, and I don\'t have much of a professional competency. Can I find a job, and how much could I be earning straight out of university?',
+      title: 'I study CS at the University of Toronto and am close to graduating with a low GPA and little work experience. Will I be able to find a job?',
       url: 'https://www.quora.com/I-study-CS-bachelors-at-U-of-T-and-I-am-close-to-graduating-with-a-low-GPA-and-I-don-not-have-much-of-a-professional-competency-Can-I-find-a-job-and-how-much-could-I-be-earning-straight-out-of-university/answer/Karel-Vuong',
       type: 2,
       date: 'Nov 03, 2014',
-      tags: [
-        "Design Analysis"
-      ]
+      tags: []
     },
     {
       id: 7,
