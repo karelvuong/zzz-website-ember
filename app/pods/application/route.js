cb8637af96762637970e521cdf89c215c514f42e
Karel Vuong <vuongkarel@gmail.com>
2015-03-21 00:25:51 -0700

    Remove goToAnchor action

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -7,18 +7,5 @@ export default Ember.Route.extend({
       answers: this.store.find('post-answer'),
       articles: this.store.find('post-article')
     };
-  }/*,
-  actions: {
-   goToAnchor: function(route, anchor) {
-      var $elem, $scrollTo;
-      this.transitionTo(route).then(function() {
-        Em.run.schedule('afterRender', function(){
-          $elem = $('#' + anchor);
-          $('html,body').animate({
-              scrollTop: $elem.offset().top - 15
-          }, 200);
-        });
-      });
-    }
-   }*/
+  }
 });
