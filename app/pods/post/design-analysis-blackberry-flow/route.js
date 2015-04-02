327dfc0dfcedb18094dfc70da5a53fd750e91c62
Karel Vuong <vuongkarel@gmail.com>
2015-04-01 17:22:25 -0700

    Fix reset-scroll on post routes

--- app/pods/post/design-analysis-blackberry-flow/route.js
+++ app/pods/post/design-analysis-blackberry-flow/route.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import resetScroll from '../../mixins/reset-scroll.js';
+import resetScroll from '../../../mixins/reset-scroll.js';
 
 export default Ember.Route.extend(resetScroll, {
   titleToken: 'Design Analysis: BlackBerry Flow',
--- app/pods/post/website-redesign/route.js
+++ app/pods/post/website-redesign/route.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import resetScroll from '../../../mixins/reset-scroll.js';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(resetScroll, {
   titleToken: 'Website Redesign',
   model: function() {
     return this.store.find('post', 1);
