eb4b87bb315ca2be101ab3503dacb6d00f10b96c
Karel Vuong <vuongkarel@gmail.com>
2015-04-10 07:40:33 -0700

    Fix page title

--- app/pods/portfolio/websites/route.js
+++ app/pods/portfolio/websites/route.js
@@ -2,5 +2,5 @@ import Ember from 'ember';
 import resetScroll from '../../../mixins/reset-scroll.js';
 
 export default Ember.Route.extend(resetScroll, {
-  titleToken: 'Extensions'
+  titleToken: 'Websites'
 });
