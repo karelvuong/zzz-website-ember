fca684b772434241991175b1b928b14d436309cb
Karel Vuong <vuongkarel@gmail.com>
2015-05-02 19:54:08 -0400

    Disable posts

--- app/pods/post/model.js
+++ app/pods/post/model.js
@@ -20,38 +20,38 @@ Post.reopenClass({
       date: 'Mar 17, 2015',
       tags: []
     },
-    {
-      id: 2,
-      title: 'Zazzle.com Settings',
-      url: 'post.design-analysis-zazzle-settings',
-      date: 'Mar 20, 2015',
-      tags: [
-        "Design Analysis"
-      ]
-    },
-    {
-      id: 3,
-      title: 'Zazzle Maker Dashboard',
-      url: 'post.design-analysis-zazzle-maker',
-      date: 'Mar 02, 2015',
-      tags: [
-        "Design Analysis"
-      ]
-    },
-    {
-      id: 4,
-      title: 'BlackBerry Flow',
-      url: 'post.design-analysis-blackberry-flow',
-      date: 'Jan 05, 2015',
-      tags: [
-        "Design Analysis"
-      ]
-    },
+    // {
+    //   id: 2,
+    //   title: 'Zazzle.com Settings',
+    //   url: 'post.design-analysis-zazzle-settings',
+    //   date: 'Mar 20, 2015',
+    //   tags: [
+    //     "Design Analysis"
+    //   ]
+    // },
+    // {
+    //   id: 3,
+    //   title: 'Zazzle Maker Dashboard',
+    //   url: 'post.design-analysis-zazzle-maker',
+    //   date: 'Mar 02, 2015',
+    //   tags: [
+    //     "Design Analysis"
+    //   ]
+    // },
+    // {
+    //   id: 4,
+    //   title: 'BlackBerry Flow',
+    //   url: 'post.design-analysis-blackberry-flow',
+    //   date: 'Jan 05, 2015',
+    //   tags: [
+    //     "Design Analysis"
+    //   ]
+    // },
     {
       id: 5,
       title: 'Joining Vidyard',
       url: 'post.announcement-joining-vidyard',
-      date: 'Apr 25, 2015',
+      date: 'May 2, 2015',
       tags: [
         "Announcement"
       ]
